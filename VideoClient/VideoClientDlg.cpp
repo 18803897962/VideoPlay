288803bd903eee2f5c72b18fad52c4ba78e2d4fd
18803897962 <1249233018@qq.com>
2023-07-17 18:36:33 +0800

    1、初步完成客户端UI设计和部分代码

--- VideoClient/VideoClientDlg.cpp
+++ VideoClient/VideoClientDlg.cpp
@@ -21,11 +21,17 @@ CVideoClientDlg::CVideoClientDlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_VIDEOCLIENT_DIALOG, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+	m_status = false;
 }
 
 void CVideoClientDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT_play, m_video);
+	DDX_Control(pDX, IDC_SLIDER_POS, m_pos);
+	DDX_Control(pDX, IDC_SLIDER_VOLUME, m_volume);
+	DDX_Control(pDX, IDC_EDIT_URL, M_URL);
+	DDX_Control(pDX, IDC_BTN_PLAY, m_btnPlay);
 }
 
 BEGIN_MESSAGE_MAP(CVideoClientDlg, CDialogEx)
@@ -33,6 +39,12 @@ BEGIN_MESSAGE_MAP(CVideoClientDlg, CDialogEx)
 	ON_WM_QUERYDRAGICON()
 	ON_WM_TIMER()
 	ON_WM_DESTROY()
+	ON_BN_CLICKED(IDC_BTN_PLAY, &CVideoClientDlg::OnBnClickedBtnPlay)
+	ON_BN_CLICKED(IDC_BTN_STOP, &CVideoClientDlg::OnBnClickedBtnStop)
+	ON_NOTIFY(TRBN_THUMBPOSCHANGING, IDC_SLIDER_POS, &CVideoClientDlg::OnTRBNThumbPosChangingSliderPos)
+	ON_NOTIFY(TRBN_THUMBPOSCHANGING, IDC_SLIDER_VOLUME, &CVideoClientDlg::OnTRBNThumbPosChangingSliderVolume)
+	ON_WM_HSCROLL()
+	ON_WM_VSCROLL()
 END_MESSAGE_MAP()
 
 
@@ -48,6 +60,14 @@ BOOL CVideoClientDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 	// TODO: 在此添加额外的初始化代码
+	SetTimer(0, 500, NULL);
+	m_pos.SetRange(0, 100);
+	m_volume.SetRange(0, 100);
+	m_volume.SetTic(10);
+	m_volume.SetTicFreq(20);
+	SetDlgItemText(IDC_STATIC_VOLUME, _T("100%"));
+	SetDlgItemText(IDC_STATIC_TIME, _T("--:--:--/--:--:--"));//设置时间文本框
+
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
@@ -94,6 +114,12 @@ void CVideoClientDlg::OnTimer(UINT_PTR nIDEvent)
 {
 	// TODO: 在此添加消息处理程序代码和/或调用默认值
 
+	if (nIDEvent == 0) {
+		//调用Controller获取播放状态以及进度信息
+		//更新IDC_STATIC_VOLUME音量
+		//更新IDC_STATIC_TIME播放时间
+	}
+
 	CDialogEx::OnTimer(nIDEvent);
 }
 
@@ -101,6 +127,84 @@ void CVideoClientDlg::OnTimer(UINT_PTR nIDEvent)
 void CVideoClientDlg::OnDestroy()
 {
 	CDialogEx::OnDestroy();
+	
+	KillTimer(0);
+}
 
-	// TODO: 在此处添加消息处理程序代码
+
+void CVideoClientDlg::OnBnClickedBtnPlay()
+{
+	if (m_status == false) {
+		m_btnPlay.SetWindowText(_T("暂停"));
+		m_status = true;
+		//TODO:调用Controller的播放接口
+	}
+	else {
+		m_btnPlay.SetWindowText(_T("播放"));
+		m_status = false;
+		//TODO:调用Controller的暂停接口
+
+	}
+}
+
+
+void CVideoClientDlg::OnBnClickedBtnStop()
+{
+	m_btnPlay.SetWindowText(_T("播放"));
+	m_status = false;
+	//TODO:调用Controller的停止接口
+}
+
+
+void CVideoClientDlg::OnTRBNThumbPosChangingSliderPos(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// 此功能要求 Windows Vista 或更高版本。
+	// _WIN32_WINNT 符号必须 >= 0x0600。
+	NMTRBTHUMBPOSCHANGING* pNMTPC = reinterpret_cast<NMTRBTHUMBPOSCHANGING*>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+
+	TRACE("pos %d reason %d\r\n", pNMTPC->dwPos, pNMTPC->nReason);
+
+	*pResult = 0;
+}
+
+
+void CVideoClientDlg::OnTRBNThumbPosChangingSliderVolume(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// 此功能要求 Windows Vista 或更高版本。
+	// _WIN32_WINNT 符号必须 >= 0x0600。
+	NMTRBTHUMBPOSCHANGING* pNMTPC = reinterpret_cast<NMTRBTHUMBPOSCHANGING*>(pNMHDR);
+	// TODO: 在此添加控件通知处理程序代码
+
+	TRACE("pos %d reason %d\r\n", pNMTPC->dwPos, pNMTPC->nReason);
+
+
+	*pResult = 0;
+}
+
+
+void CVideoClientDlg::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	//TODO:视频进度条待vlc获取媒体之后才能完成
+	TRACE("pos %p column %p cur %p pos %d\r\n", &m_pos, &m_volume, pScrollBar, nPos);
+	if (nSBCode == 5) {
+		CString strVolume;
+		strVolume.Format(_T("%d%%"), nPos);
+		SetDlgItemText(IDC_STATIC_TIME, strVolume);//设置时间文本框
+	}
+	CDialogEx::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CVideoClientDlg::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	if (nSBCode == 5) {//由于某种原因在鼠标放开的一瞬间，nPos会变为0，怀疑可能跟nSBCode有关，测试正常情况下nSBCode为5
+		TRACE("pos %p column %p cur %p pos %d\r\n", &m_pos, &m_volume, pScrollBar, nPos);
+		CString strVolume;
+		strVolume.Format(_T("%d%%"), 100 - nPos);
+		SetDlgItemText(IDC_STATIC_VOLUME, strVolume);//设置声音文本框
+	}
+	CDialogEx::OnVScroll(nSBCode, nPos, pScrollBar);
 }
--- VideoClient/VideoClientDlg.h
+++ VideoClient/VideoClientDlg.h
@@ -24,7 +24,7 @@ public:
 // 实现
 protected:
 	HICON m_hIcon;
-
+	bool m_status;//暂停状态为true 播放状态为false
 	// 生成的消息映射函数
 	virtual BOOL OnInitDialog();
 	afx_msg void OnPaint();
@@ -33,4 +33,15 @@ protected:
 public:
 	afx_msg void OnTimer(UINT_PTR nIDEvent);
 	afx_msg void OnDestroy();
+	CEdit m_video;
+	CSliderCtrl m_pos;
+	CSliderCtrl m_volume;
+	CEdit M_URL;
+	afx_msg void OnBnClickedBtnPlay();
+	CButton m_btnPlay;
+	afx_msg void OnBnClickedBtnStop();
+	afx_msg void OnTRBNThumbPosChangingSliderPos(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnTRBNThumbPosChangingSliderVolume(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
 };
